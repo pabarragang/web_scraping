1d922988057dd124f57293274bde688dc3c2bf22
Andrea Barragan <andreabarragan@ticsocial.com.co>
2018-05-15 17:39:11 -0500

    Http request previous

--- main.py
+++ main.py
@@ -29,13 +29,39 @@ class Scraping:
 
         return f.read()
 
+    def make_request_survey(self,url,start_date,end_date):
+        #the http headers are useful to simulate a particular browser (some sites deny
+        #access to non-browsers (bots, etc.)
+        #also needed to pass the content type.
+        headers = {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+            'Accept-Encoding':'gzip, deflate',
+            'Accept-Language':'es-ES,es;q=0.8,en;q=0.6',
+            'Cache-Control':'max-age=0',
+            'Connection':'keep-alive',
+            'Content-Length':'7434',
+            'Content-Type':'application/x-www-form-urlencoded',
+            'Cookie': 'ASP.NET_SessionId='+self.sessionId+'; StationParam=START_DATE='+start_date+' 1:00:00&START_TIME=01:00&END_DATE='+end_date+' 0:00:00&END_TIME=00:00',
+            'Host': '201.245.192.252:81',
+            'Origin': 'http://201.245.192.252:81',
+            'Referer': url,
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.170 Safari/537.36'
+        }
+
+        with open('encoding.txt', 'r') as myfile:
+            data=myfile.read().replace('\n', '')
+        req = urllib2.Request(url, data, headers)
+        f= urllib2.urlopen(req)
+
+        return f.read()
+
     def get_beautiful_response(self,text_plain,id_item):
         soup = BeautifulSoup(text_plain, 'html.parser')
         return BeautifulSoup(str(soup.find_all(id=id_item)[0]), 'html.parser')
 
     def get_monitors_values(self,table,hour):
         index = 26+((hour-1)*13)
-        print index
         results = {}
         table = table.find_all("td")
         results["Datetime"] = table[index].get_text()
@@ -56,6 +82,7 @@ class Scraping:
 
 
 scraping = Scraping("http://201.245.192.252:81/NewGrid.aspx","03esqlbye05mofnphel0bo25")
+print scraping.make_request_survey("http://201.245.192.252:81/frmStationReport.aspx","14-05-2018","15-05-2018")
 text_plain = scraping.make_request_report("14-05-2018","15-05-2018")
 table = scraping.get_beautiful_response(text_plain,"C1WebGrid1")
 response = scraping.get_monitors_values(table,1)
